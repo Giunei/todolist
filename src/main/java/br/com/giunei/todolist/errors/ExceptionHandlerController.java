5220467922c51ec5049bf6a78952b6819c415157
giunei <pmjgiunei@gmail.com>
2023-10-16 23:17:10 -0300

    Correção controller handler

--- src/main/java/br/com/giunei/todolist/errors/ExceptionHandlerController.java
+++ src/main/java/br/com/giunei/todolist/errors/ExceptionHandlerController.java
@@ -1,4 +1,4 @@
-package errors;
+package br.com.giunei.todolist.errors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/br/com/giunei/todolist/task/TaskModel.java
+++ src/main/java/br/com/giunei/todolist/task/TaskModel.java
@@ -34,9 +34,8 @@ public class TaskModel {
 
     public void setTitle(String title) throws Exception {
         if(title.length() > 50) {
-            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+            throw new Exception("O campo de título deve conter no máximo 50 caracteres");
         }
         this.title = title;
     }
-
 }
